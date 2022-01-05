a652e4d8e50b1c600cf9ea45bea2360668363793
dlshnkLK <dilshankaliyanage@yahoo.com>
2022-01-06 00:50:33 +0530

    init

---
+++ pages/Header.js
@@ -0,0 +1,31 @@
+import NextLink from 'next/link';
+
+const Header = () => {
+	return (
+		
+		<div className="flex flex-row justify-centeir md:pt-12 xl:pt-16 md:mx-36">
+			<div className="w-5/12">
+				<div className="w-72 h-auto">
+  					<NextLink href="/"><a href="/"><img src="/Resources/About/Logo.png" alt="logo" /></a></NextLink>
+				</div>
+			</div>
+
+			<div className="w-7/12 text-f18 Inter-Regular text-gray-200">
+				<ul className="flex flex-row justify-center space-x-6 ml-24">
+					<li className=""><NextLink href="/capabilities"><a href="/capablities">Capabilities</a></NextLink></li>
+					<li className=""><NextLink href="/case-study"><a href="/case-study">Case Studies</a></NextLink></li>
+					<li className=""><NextLink href="/about-us"><a href="/about-us">About</a></NextLink></li>
+					<li className=""><NextLink href="/contact-us"><a href="/contact-us">Contact</a></NextLink></li>
+				</ul>
+			
+			</div>
+
+		</div>
+
+	);
+
+}
+
+export default Header;
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+	fontSize: {
+		'f50': '3.125rem',
+		'f25': '1.5625rem',
+		'f24': '1.5rem',
+		'f18': '1.125rem',
+		'f16': '1rem',
+		'f14': '0.875rem',
+	},
+	colors: {
+		'hero-dark': '#07152D',
+		'home-cap':'#5648FF',
+	},
+	lineHeight: {
+		'f50' : '66px',
+		'f24' : '36px',
+	},
+	spacing: {
+		'hero-tagline-spacing' : '30rem',
+	},
+	margin: {
+		'temp': '22.5rem',
+	},
+    },
+  },
+  plugins: [],
+}
